2a5b370e0ca4d92b2ce1f8321692b986dcdace9d
nemanjalugi <nemanjalugi@gmail.com>
2020-11-15 11:14:18 +0100

    Separate app strings

--- lib/config.dart
+++ lib/config.dart
@@ -3,3 +3,15 @@ import 'package:flutter/material.dart';
 class AppColors {
   static Color green = Color(0xff1BAA53);
 }
+
+class Strings {
+  static String wonTitle = '';
+  static String loseTitle = '';
+  static String wonDescription = '';
+  static String loseDescription = '';
+  static String homeAction = 'HOME';
+  static String restartAction = 'RESTART';
+  static String startAction = 'START';
+  static String appTitle = 'Memory game';
+  static String appWelcomeText = 'Welcome to super cool Memory Game';
+}
